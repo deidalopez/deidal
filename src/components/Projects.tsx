61ceb6047e065ce0dac977d2d77c7417015b30fb
deida <deidalopez1@gmail.com>
2024-05-01 09:51:49 -0700

    Less repetitive structure

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,86 +3,79 @@ import React from "react";
 import Image from "next/image";
 import ReactCardFlip from "react-card-flip";
 
-const Project = ({
-  name,
-  image,
-  description,
+const projects = [
+  {
+    name: "Care/of",
+    image: "/images/Careof/CO_track.png",
+    description: "Care/of: Healthy Habits Tracker",
+  },
+  {
+    name: "Care/of Sleep",
+    image: "/images/Ashwa/Ashwa_home.png",
+    description: "Care/of: Sleep",
+  },
+  {
+    name: "West Tenth",
+    image: "/images/WestTenth/W10_home.png",
+    description: "West Tenth: Marketplace",
+  },
+];
+
+const Card = ({
+  project,
+  isFlipped,
+  handleHover,
 }: {
-  name: string;
-  image: string;
-  description: string;
+  project: { image: string; description: string };
+  isFlipped: boolean;
+  handleHover: () => void;
+}) => (
+  <div className="border h-80 w-40 mb-4 justify-content">
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div onMouseEnter={handleHover}>
+        <Image
+          src={project.image}
+          alt="deida image"
+          width={180}
+          height={400}
+          className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+        />{" "}
+      </div>
+
+      <div className="background-white h-300 w-300" onMouseLeave={handleHover}>
+        <p>{project.description}</p>
+      </div>
+    </ReactCardFlip>
+  </div>
+);
+
+const Project = ({
+  project,
+}: {
+  project: { name: string; image: string; description: string };
 }) => {
   const [isFlipped, setIsFlipped] = React.useState(false);
   const handleHover = () => {
     setIsFlipped((prev) => !prev);
   };
 
-  const Card = () => (
-    <div className="border h-40 w-40 mb-4">
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-        <div onMouseEnter={handleHover}>
-          <Image
-            src="/images/circle-profile.png"
-            alt="deida image"
-            width={300}
-            height={300}
-            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-          />{" "}
-        </div>
-
-        <div
-          className="background-white h-300 w-300"
-          onMouseLeave={handleHover}
-        >
-          <p>{description}</p>
-        </div>
-      </ReactCardFlip>
-    </div>
-  );
-
   return (
     <div>
-      {/* <h3>{name}</h3> */}
-      <div >{Card()} </div>
+      <h3>{project.name}</h3>
+      <div className="flex justify-center">
+        {Card({ project, isFlipped, handleHover })}{" "}
+      </div>
     </div>
   );
 };
 
 const Projects = () => {
-  const projects = [
-    {
-      name: "Project 1",
-      image: "/images/circle-profile.png",
-      description: "This is project 1",
-    },
-    {
-      name: "Project 2",
-      image: "/images/circle-profile.png",
-      description: "This is project 2",
-    },
-    {
-      name: "Project 3",
-      image: "/images/circle-profile.png",
-      description: "This is project 3",
-    },
-    {
-      name: "Project 4",
-      image: "/images/circle-profile.png",
-      description: "This is project 4",
-    },
-  ];
-
   return (
     <section>
       <h2 className="text-3xl font-bold mb-6">Projects</h2>
-      <div>
-        {projects.map((item, index) => (
-          <Project
-            key={index}
-            name={item.name}
-            image={item.image}
-            description={item.description}
-          />
+      <div className="flex">
+        {projects.map((project, index) => (
+          <Project key={index} project={project} />
         ))}
       </div>
     </section>
