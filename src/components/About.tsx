80a15776c2e98115718eb7c9335e01489c114b3d
deida <deidalopez1@gmail.com>
2024-05-07 13:33:49 -0700

    About update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,20 +1,26 @@
 import React from "react";
+import ProfilePhoto from "./ProfilePhoto";
 
 const About = () => {
   return (
     <section className="text-white">
-      <h2 className="text-3xl font-bold mb-6">About Me</h2>
-      <p>
-        I am passionate about mobile development using React Native, and
-        providing sleek solutions, intuitive UI, and overall great experiences
-        for users. I value and I am dedicated to provide well documented code,
-        metrics and results for the company.
-      </p>
-      <br />
-      <p>
-        I also have experience working as a Full Stack Developer for a start-up
-        using React, Node.js, GraphQL, and PostgreSQL
-      </p>
+      <div className="flex flex-row flex-wrap gap-4">
+        <div className="flex-1">
+          <h2 className="text-3xl font-bold mb-6">About Me</h2>
+          <p>
+            I am passionate about mobile development using React Native, and
+            providing sleek solutions, intuitive UI, and overall great
+            experiences for users. I value and I am dedicated to provide well
+            documented code, metrics and results for the company.
+          </p>
+          <br />
+          <p>
+            I also have experience working as a Full Stack Developer for a
+            start-up using React, Node.js, GraphQL, and PostgreSQL
+          </p>
+        </div>
+        <ProfilePhoto />
+      </div>
     </section>
   );
 };
