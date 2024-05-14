68662fb5a523980737e1b71ba1c5ebc016f2a77c
deida <deidalopez1@gmail.com>
2024-05-13 23:13:43 -0700

    slight styling

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -3,7 +3,7 @@ import ProfilePhoto from "../components/ProfilePhoto";
 
 const About = () => {
   return (
-    <div>
+    <div className="container mt-12 mx-auto px-10 py-10">
       <h1>About me</h1>
       <ProfilePhoto />
       <p>
@@ -15,8 +15,7 @@ const About = () => {
         2020, when I began taking Computer Science courses at a community
         college. This led me to enroll in a coding bootcamp, where I learned the
         fundamentals of web development.
-        <br />
-        I gained a lot of experience working at a fast-paced startup,
+        <br />I gained a lot of experience working at a fast-paced startup,
         where I was able to see how my work directly impacted the company's
         success.
       </p>
