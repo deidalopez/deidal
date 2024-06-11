5edaf77ddbbb07202f7fa598c196573a5b591b97
deida <deidalopez1@gmail.com>
2024-06-11 14:18:24 -0700

    About section breakpoints

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -6,17 +6,20 @@ const About = () => {
     <section className="text-white">
       <div className="flex flex-row flex-wrap gap-4">
         <div className="flex-1">
-          <h2 className="text-3xl font-bold mb-6">About Me</h2>
-          <p>
-            I am passionate about mobile development using React Native, and
-            providing sleek solutions, intuitive UI, and overall great
-            experiences for users. I value and I am dedicated to provide well
-            documented code, metrics and results for the company.
-          </p>
-          <br />
-          <p>
-            I also have experience working as a Full Stack Developer for a
-            start-up using React, Node.js, GraphQL, and PostgreSQL
+          <h2 className="text-3xl font-bold mb-4 md:text-4xl lg:text-5xl">
+            About Me
+          </h2>
+          <p className="md:text-lg lg:text-2xl">
+            I am passionate about providing sleek solutions, intuitive UI, and
+            an overall great experiences for users. I value and am dedicated to
+            well documented code, keeping track of metrics, providing meaningful
+            results for the company.
+            <br />
+            <p className="my-4">
+              I have experience working as a Full Stack Developer using React,
+              Node.js, GraphQL, and PostgreSQL, and as a Mobile Developer using
+              React Native.
+            </p>
           </p>
         </div>
         <ProfilePhoto />
