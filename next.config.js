20b5b7f4add990794958937d3c1d2a051fa86852
deida <deidalopez1@gmail.com>
2024-06-17 15:55:42 -0700

    basePath added

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  output: "export",
 
+const isProcess = process.env.NODE_ENV === "production";
+const nextConfig = {
+  basePath: isProcess ? "/deidal-nextjs" : "",
   // Optional: Change the output directory `out` -> `dist`
   distDir: "dist",
-
   images: {
     unoptimized: true,
   },
+  output: "export",
 };
 
 module.exports = nextConfig;
