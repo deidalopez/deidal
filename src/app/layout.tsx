6c3d028a6683256a4a6cb4ec6a962fbe56b7d705
Deida Lopez <deidalopez1@gmail.com>
2024-08-28 15:19:01 -0700

    Toggle theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import NavBar from "@/app/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`bg-bkg text-content ${inter.className}`}>
+        <Providers>
+          <NavBar />
+          <main className="flex flex-col min-h-screen px-4 md:px-6">
+            {children}
+          </main>
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
 import HeroSection from "@/app/components/Hero";
 import Contact from "@/app/components/Contact";
 import Skills from "@/app/components/Skills";
-import NavBar from "@/app/components/NavBar";
+// import NavBar from "@/app/components/NavBar";
 import About from "@/app/components/About";
 import Projects from "@/app/components/Projects";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="container mx-auto px-12 py-4">
-        <NavBar />
-        <div className="container mt-24 mx-auto">
-          <HeroSection />
-          <About />
-          <br />
-          <Skills />
-          <br />
-          <Projects />
-          <br />
-          <Contact />
-        </div>
+    <div className="container mx-auto px-12 py-4">
+      {/* <NavBar /> */}
+      <div className="container mt-24 mx-auto">
+        <HeroSection />
+        <About />
+        <br />
+        <Skills />
+        <br />
+        <Projects />
+        <br />
+        <Contact />
       </div>
-    </main>
+    </div>
   );
 }
